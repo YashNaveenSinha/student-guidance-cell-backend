a757d89e7eda33afbc6cd5cf0a6d56fc98a3c79e
Yash Naveen Sinha <yashnsinha@gmail.com>
2022-04-02 21:49:37 +0530

    backend added

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+     mongoURI: "mongodb://Team14SELab:j31m15m31@cluster0-shard-00-00.ieude.mongodb.net:27017,cluster0-shard-00-01.ieude.mongodb.net:27017,cluster0-shard-00-02.ieude.mongodb.net:27017/student-guidance-cell?ssl=true&replicaSet=atlas-shutdf-shard-0&authSource=admin&retryWrites=true&w=majority"//"mongodb+srv://Team14SELab:j31m15m31@cluster0.ieude.mongodb.net/student-guidance-cell?retryWrites=true&w=majority" //"mongodb://localhost:27017/doctorconnectorDB"
+};
+
+//mongodb://Team14SELab:<password>@cluster0-shard-00-00.ieude.mongodb.net:27017,cluster0-shard-00-01.ieude.mongodb.net:27017,cluster0-shard-00-02.ieude.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-shutdf-shard-0&authSource=admin&retryWrites=true&w=majority
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const doctors = require('./routes/api/doctors');
+const authsDoctor = require('./routes/api/authDoctor');
+const users = require('./routes/api/users');
+const authsUser = require('./routes/api/authUser');
+const profile = require('./routes/api/profile');
+const appointment = require('./routes/api/appointment');
+
+const app = express();
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// connect to MongoDB;
+mongoose
+    .connect("mongodb://localhost:27017", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
+//Init Middleware 
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send("Welcome Team 14 and NITC Student Guidance Cell"));
+
+// Use Routes
+app.use('/api/doctors', doctors);
+app.use('/api/authDoctor', authsDoctor);
+app.use('/api/users', users);
+app.use('/api/authUser', authsUser);
+app.use('/api/profile', profile);
+app.use('/api/appointment', appointment);
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+//j31m15m31
